66ca26b3c09bc0ca74ddd34006e2779973956274
Jacques Marneweck <jacques@powertrip.co.za>
2016-02-19 09:45:33 +0200

    Bootstrap file for tests

---
+++ tests/bootstrap.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Test Suite for Standard Bank Statement Parsers
+ *
+ * THIS IS UNPUBLISHED PROPRIETARY SOURCE CODE OF JACQUES MARNEWECK
+ * The copyright notice above does not evidence any
+ * actual or intended publication of such source code.
+ *
+ * Redistribution of this material is strictly prohibited.
+ *
+ * @author    Jacques Marneweck <jacques@powertrip.co.za>
+ * @copyright 2014-2016 Jacques Marneweck.  All rights reserved.
+ * @package   Plutus
+ */
+
+require_once __DIR__.'/../vendor/autoload.php';
